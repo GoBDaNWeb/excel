1b88de899ab24cfb7fef5270e8e3d22cd568f3fd
Bogdan <bogdan.ognitsiy@yandex.ru>
2021-03-18 20:29:25 +0300

    redux logic

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,48 +1,46 @@
-import {ExcelComponent} from 'core/ExcelComponent';
+import {ExcelStateComponent} from 'core/ExcelStateComponent'
+import {createToolbar} from '@/components/toolbar/toolbar.template'
+import {$} from 'core/Dom'
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
     static className = 'excel__toolbar'
 
     constructor($root, options) {
         super($root, {
             name: 'Toolbar',
-            listeners: [],
+            listeners: ['click'],
             ...options
         })
     }
 
+    prepare() {
+        const initialState = {
+            textAlign: 'left',
+            fontWeight: 'normal',
+            textDecoration: 'none',
+            fontStyle: 'normal'
+        }
+        this.initState(initialState)
+    }
+
+    get template() {
+        return createToolbar(this.state)
+    }
+
     toHTML() {
-        return `
-         <div class="button">
-                        <span class="material-icons">
-                            format_bold
-                        </span>
-                </div>
-                <div class="button">
-                        <span class="material-icons">
-                            format_italic
-                        </span>
-                </div>
-                <div class="button">
-                        <span class="material-icons">
-                            format_underlined
-                        </span>
-                </div>
-                <div class="button">
-                        <span class="material-icons">
-                            format_align_left
-                        </span>
-                </div>
-                <div class="button">
-                        <span class="material-icons">
-                           format_align_center
-                        </span>
-                </div>
-                <div class="button">
-                        <span class="material-icons">
-                            format_align_right
-                        </span>
-                </div>
-        `
+        return this.template
+    }
+
+    onClick(event) {
+        const $target = $(event.target)
+        if ($target.data.type === 'button') {
+            const value = JSON.parse($target.data.value)
+            const key = Object.keys(value)[0]
+            this.$emit('toolbar:applyStyle', value)
+
+            this.setState({[key]: value[key]})
+            console.log(this.state)
+            console.log(value)
+        }
     }
 }
